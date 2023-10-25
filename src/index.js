17790073c33e2913b9aecb83729ffd22b769350e
chrjl <73675826+chrjl@users.noreply.github.com>
2023-10-24 23:08:37 -0700

    feat(Skills,Languages): definition lists

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { Skills, Languages } from './sections/index.jsx';
+
 const sections = [
+  { id: 'skills', Component: Skills },
+  { id: 'languages', Component: Languages },
 ];
 
 export default sections;
---
+++ src/sections/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export function Skills({ data }) {
+  return (
+    <>
+      <h1>Skills</h1>
+      {data &&
+        data.map(({ title, keywords }) => (
+          <>
+            <dl>
+              <dt>{title}</dt>
+              <dl>{keywords.join(', ')}</dl>
+            </dl>
+          </>
+        ))}
+    </>
+  );
+}
+
+export function Languages({ data }) {
+  console.log(data);
+  return (
+    data && (
+      <dl>
+        <dt>Languages</dt>
+        <dd>{data.keywords.join(', ')}</dd>
+      </dl>
+    )
+  );
+}
