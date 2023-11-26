76f752dc6532ef229e9a70744496f5e91cbd76ee
chrjl <73675826+chrjl@users.noreply.github.com>
2023-11-25 16:40:18 -0800

    feat: add DebugComponents
    - Education
    - Certificates
    - Projects
    - Work
    - Experience

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import React from 'react';
 
-import { Skills, Languages, TemplateComponentProps } from './sections';
+import {
+  Skills,
+  Languages,
+  Education,
+  Certificates,
+  Projects,
+  Experience,
+  Work,
+  TemplateComponentProps,
+} from './sections';
 
 const sections: TemplateSection[] = [
   { id: 'skills', Component: Skills },
   { id: 'languages', Component: Languages },
+  { id: 'education', Component: Education },
+  { id: 'certificates', Component: Certificates },
+  { id: 'projects', Component: Projects },
+  { id: 'experience', Component: Experience },
+  { id: 'work', Component: Work },
 ];
 
 export default sections;
--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ResumeEntry } from '@reactresume/types';
-import { DefinitionTable } from '@reactresume/components';
+import { DefinitionTable, DebugComponent } from '@reactresume/components';
 
 export interface TemplateComponentProps {
   data: ResumeEntry[];
@@ -20,5 +20,40 @@ export const Languages = ({ data }: TemplateComponentProps) => (
   <>
     <h1>Languages</h1>
     <DefinitionTable data={data} />
+  </>
+);
+
+export const Education = ({ data }: TemplateComponentProps) => (
+  <>
+    <h1>Education</h1>
+    <DebugComponent data={data} />
+  </>
+);
+
+export const Certificates = ({ data }: TemplateComponentProps) => (
+  <>
+    <h1>Certificates</h1>
+    <DebugComponent data={data} />
+  </>
+);
+
+export const Projects = ({ data }: TemplateComponentProps) => (
+  <>
+    <h1>Projects</h1>
+    <DebugComponent data={data} />
+  </>
+);
+
+export const Work = ({ data }: TemplateComponentProps) => (
+  <>
+    <h1>Work</h1>
+    <DebugComponent data={data} />
+  </>
+);
+
+export const Experience = ({ data }: TemplateComponentProps) => (
+  <>
+    <h1>Experience</h1>
+    <DebugComponent data={data} />
   </>
 );
