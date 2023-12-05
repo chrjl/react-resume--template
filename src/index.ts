19ba4cea8ed347aff363f2467a3b62dcf4ac1538
chrjl <73675826+chrjl@users.noreply.github.com>
2023-12-04 22:21:25 -0800

    fix: reorder sections

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ const basics = { id: 'basics', Component: Basics };
 const sections: TemplateSection[] = [
   { id: 'skills', Component: Skills },
   { id: 'languages', Component: Languages },
+  { id: 'work', Component: Work },
   { id: 'projects', Component: Projects },
   { id: 'experience', Component: Experience },
   { id: 'education', Component: Education },
   { id: 'certificates', Component: Certificates },
-  { id: 'work', Component: Work },
 ];
 
 export default [basics, ...sections];
